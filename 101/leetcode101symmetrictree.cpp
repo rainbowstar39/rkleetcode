4245d4706be9d74c20818296c974ba9883afef0f
rk <shiningciel@gmail.com>
2016-11-01 17:46:58 +0800

    101. Symmetric Tree(modify coding style)
    ver2

--- 101/leetcode101symmetrictree.cpp
+++ 101/leetcode101symmetrictree.cpp
@@ -22,34 +22,29 @@ using namespace std;
   }; 
 class Solution {
 public:
-	bool comp(TreeNode *a,TreeNode *b){
-//		cout<<endl<<a->val<<","<<b->val<<endl;
+	bool comp(TreeNode* a, TreeNode* b){
 		if(a==NULL && b==NULL) return true;
-		if(a!=NULL && b!=NULL && a->val!=b->val) return false;
-		if(a!=NULL && b==NULL) return false;
-		if(a==NULL && b!=NULL) return false; 
-//		if( a->val!=b->val ) return false;
-//		 if(a==NULL || b==NULL) return false;
-		
-		 
+		if(a==NULL) return false;
+		if(b==NULL) return false;
+		if(a->val!=b->val) return false;
 		
 		return comp(a->left,b->right) && comp(a->right,b->left);
 	}
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return true;
-		return comp(root->left,root->right);       
-
+		if(root==NULL) return true;
+	
+		return comp(root->left,root->right) ;
     }
 };
 int main(void){
 	Solution aa;
 	TreeNode *root=new TreeNode(1);
 	root->left=new TreeNode(2);
-	//root->right=new TreeNode(2);
-//	root->left->left=new TreeNode(3);
-//	root->left->right=new TreeNode(4);
+	root->right=new TreeNode(2);
+	//root->left->left=new TreeNode(3);
+	root->left->right=new TreeNode(3);
 //	root->right->left=new TreeNode(4);
-//	root->right->right=new TreeNode(3);
+	root->right->right=new TreeNode(3);
 	cout<<aa.isSymmetric(root);
 	return 0;
 }
