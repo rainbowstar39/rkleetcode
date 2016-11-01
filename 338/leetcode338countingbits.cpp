413664665297260819670d51a86222c92af94be9
rk <shiningciel@gmail.com>
2016-11-01 22:41:05 +0800

    338. Counting Bits(improve perfomance)
    128ms->102ms

--- 338/leetcode338countingbits.cpp
+++ 338/leetcode338countingbits.cpp
@@ -8,37 +8,24 @@ using namespace std;
 //DATE:2016/04/29    TIME:03:30:32
 //DATE:2016/04/29    TIME:04:14:43
 //DATE:2016/04/29    TIME:04:40:53
+//DATE:2016/11/01    TIME:22:40:06
 class Solution {
 public:
-	int getcntbit(int i){
-		int cnt=0;
-		
-		for(int idx=i;idx>=1;idx/=2){
-			if(idx%2==1) cnt++;
-		}
-		return cnt;		
-	}
+
     vector<int> countBits(int num) {
-    	vector<int> rr; 
-    	rr.push_back(0);
-    	double x;
-    	int preidx=1;
-    	if(num==0) return rr; 
-    	rr.push_back(1);
-  		for(int idx=2;idx<=num;idx++){
-  			
-			if (idx>>1==preidx && idx%preidx==0){
-  			//	printf("\n%s %d %d\n",__FUNCTION__,__LINE__,idx);
-  				rr.push_back(1);
-  				preidx=idx;
-  				
+    	vector<int> rr(num+1,0); 
+    	if(num==0) return rr;
+		int preidx=2;
+    	for(int idx=1;idx<=num;idx++){
+    		if(idx==1)rr[idx]=1;
+    		if(idx==2)rr[idx]=1;
+    		if((idx-preidx)<preidx)rr[idx]=rr[preidx]+rr[idx-preidx];
+    		else if( (idx-preidx)==preidx) {
+    			preidx=idx;
+    			rr[idx]=1;
 			}
-			else{
-				int tmp;
-				tmp=rr[preidx]+rr[idx-preidx];
-				rr.push_back(tmp);
-			}
-		  }      
+		}
+    	
 		return rr;
     }
 };
