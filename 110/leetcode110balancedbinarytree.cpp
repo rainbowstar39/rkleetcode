d493f1c88d8b7ae6bfa4bb1b24deebc3e8b3e8d7
rk <shiningciel@gmail.com>
2016-11-01 17:24:08 +0800

    110. Balanced Binary Tree( from discussion)
    16ms->64ms
    not so good

--- 110/leetcode110balancedbinarytree.cpp
+++ 110/leetcode110balancedbinarytree.cpp
@@ -25,13 +25,23 @@ class Solution {
 public:
 	int height(TreeNode* root){
 		if(root==NULL) return 0;
+		int leftheight;
+		int rightheight;
+		leftheight=height(root->left);
+		if(leftheight==-1)return -1;
+		rightheight=height(root->right);
+		if(rightheight==-1) return -1;
+
+		if(abs(leftheight-rightheight)>1) return -1;
 		return max(height(root->left),height(root->right)) +1;
+		
+		
+		
 	}
     bool isBalanced(TreeNode* root) {
     	if(root==NULL) return true;
     	//if(root->left==NULL && root->right==NULL) return true;
-    	return isBalanced(root->left ) && isBalanced(root->right) && 
-    	       (abs(height(root->left)-height(root->right))<=1? 1:0);
+    	return height(root)>0? 1:0;
 
     }
     
@@ -44,9 +54,9 @@ int main(void){
 	root->left->left=new TreeNode(3);
 	root->right=new TreeNode(4);
 	root->right->right=new TreeNode(5);
-	root->right->right->right=new TreeNode(6);
+	//root->right->right->right=new TreeNode(6);
 	Solution aa;
-	cout<<aa.height(root)<<endl;
+//	cout<<aa.height(root)<<endl;
 	cout<<aa.isBalanced(root);
 	return 0;
 }
