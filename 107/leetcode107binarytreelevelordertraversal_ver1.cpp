3c030e9572a97b9a4105d232ff639a8bc4f4bfa0
rk <shiningciel@gmail.com>
2016-11-01 18:11:36 +0800

    107. Binary Tree Level Order Traversal II
    Given a binary tree, return the bottom-up level order traversal of its
    nodes' values. (ie, from left to right, level by level from leaf to
    root).
    
    For example:
    Given binary tree [3,9,20,null,null,15,7],
    3
    / \
    9  20
    /  \
    15   7
    return its bottom-up level order traversal as:
    [
    [15,7],
    [9,20],
    [3]
    ]

---
+++ 107/leetcode107binarytreelevelordertraversal_ver1.cpp
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<vector>
+using namespace std;
+//DATE:2016/04/27    TIME:12:15:56
+//DATE:2016/04/27    TIME:12:49:21
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  }; 
+class Solution {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode* root) {
+    	vector< vector<int> > rr;
+    	if(root==NULL) return rr;
+    	
+    	vector<int> tt,tt2;
+    	vector<TreeNode*> ss,ss2;
+		int idx;
+		ss.push_back(root);
+		tt.push_back(root->val);
+		rr.push_back(tt);
+	    	
+    	if(root->left==NULL && root->right==NULL)
+        return rr;
+    
+        while(ss.size()>0){
+    		ss2.clear();   
+			tt.clear(); 	
+        	for(idx=0;idx<ss.size();idx++){
+        		if(ss[idx]->left){
+        		//	cout<<ss[idx]->left->val<<",";
+					tt.push_back(ss[idx]->left->val);
+					ss2.push_back(ss[idx]->left);
+				}
+        		if(ss[idx]->right){
+        		//	cout<<ss[idx]->right->val<<",";
+					tt.push_back(ss[idx]->right->val);
+					ss2.push_back(ss[idx]->right);
+				}
+			}
+			if(ss2.size()>0){
+				ss=ss2;
+				rr.insert(rr.begin(),tt);//beat 1.54%,  
+	
+			} 
+			else {
+				break;
+			}
+			
+		}
+		
+		return rr;
+        
+    }
+};
+
+int main(void){
+	
+	Solution aa;
+	TreeNode* root; 
+	root=new TreeNode(1);
+	root->left=new TreeNode(3);
+	root->right=new TreeNode(4);
+//	root->left->left=new TreeNode(5);
+	root->right->right=new TreeNode(7);
+	vector< vector<int> > rr;
+	vector<int> tmp;
+	rr=aa.levelOrderBottom(root);
+
+	cout<<endl<<"---------ans--------------"<<endl;
+	for (int idx=0;idx<rr.size();idx++){
+		tmp=rr[idx];
+		for(int jdx=0;jdx<tmp.size();jdx++)
+		cout<<tmp[jdx]<<",";
+	}
+	return 0;
+}
