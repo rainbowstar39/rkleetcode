e376a79a8c6f539343ca0a607223509542484e67
rk <shiningciel@gmail.com>
2016-11-03 18:06:57 +0800

    231. Power of Two(8ms->3ms)
    performance improved

--- 231/leetcode231poweroftwo.cpp
+++ 231/leetcode231poweroftwo.cpp
@@ -12,15 +12,21 @@ using namespace std;
 //15:38~15:43
 //use double to calculate
 bool isPowerOfTwo(int n) {
-	if(n<0)return 0;
-	    if(n==0) return 0;
-		if(n==1) return 1;
-        else if(n%2==1)return 0;
-        if(n/2>1)return isPowerOfTwo(n/2);
+	if(n<=0)return false;
+	while(n>0){
+		if(n==1) return true;
+		else if(n&1)return false;
+		n=n>>1;
+		
+	}
+	return true;
+	/*
+	if(n/2>1)return isPowerOfTwo(n/2);
         else if(n/2==1) return 1;
+*/
 }
 int main(void){
 
-	cout<<isPowerOfTwo(512);
+	cout<<isPowerOfTwo(3072);
 	
 }
