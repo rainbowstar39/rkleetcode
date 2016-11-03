40a7872c3ade0b283939b82672c9ca5323002025
rk <shiningciel@gmail.com>
2016-11-03 18:39:25 +0800

    292. Nim Game
    You are playing the following Nim Game with your friend: There is a heap
    of stones on the table, each time one of you take turns to remove 1 to 3
    stones. The one who removes the last stone will be the winner. You will
    take the first turn to remove the stones.
    
    Both of you are very clever and have optimal strategies for the game.
    Write a function to determine whether you can win the game given the
    number of stones in the heap.
    
    For example, if there are 4 stones in the heap, then you will never win
    the game: no matter 1, 2, or 3 stones you remove, the last stone will
    always be removed by your friend.

---
+++ 292/leetcode292nimgames.cpp
@@ -0,0 +1,27 @@
+// 引入標準程式庫中相關的字串程式
+#include <string>
+ 
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+// std 為標準程式庫的名稱空間
+using namespace std;
+//15:38~15:43
+//use double to calculate
+
+
+class Solution {
+public:
+	bool canWinNim(int n) {
+		return (n%4);
+	}
+};
+int main(void){
+	Solution aa;
+
+	return 0;
+	
+}
